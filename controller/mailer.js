5ea100214745e522ac241080b7e180d2c311b33b
KhallilB <khallil.bailey@gmail.com>
2020-01-27 17:46:07 -0800

    env  & ignore

--- controller/mailer.js
+++ controller/mailer.js
@@ -1,9 +1,11 @@
 const nodeMailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'khallilbailey@gmail.com',
-    pass: 'yourpassword'
-  }
-});
+module.exports.sendEmail = async (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'khallilbailey@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
+};
